75061d19932dca574e3e2c73b223758be8221082
Kmamrutkar91 <60057262+Kmamrutkar91@users.noreply.github.com>
2022-02-08 15:52:03 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
+
 #QR Code Generator
-=======
-#QR CODE GENERATOR
->>>>>>> 15dcad882313666c09cbe25a9ddf4a4f1a5af683
 import pyqrcode
 from pyqrcode import QRCode
 s="https://youtube.com/channel/UCdto6jnxyQR2tjRt2DtnfXw"
